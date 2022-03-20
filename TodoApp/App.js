17fd65ad3869e384147a5c73e6d260da280cffb4
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-03-20 21:47:17 +0900

    RN study

---
+++ TodoApp/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import DateHead from './components/DateHead';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+
+function App() {
+  const today = new Date();
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']}>
+        <DateHead date={today} />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
